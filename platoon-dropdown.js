6ce0c70fc803eb5e5a8e5871047296c5c868f695
Daniel Peukert <dan.peukert@gmail.com>
2013-12-06 11:06:48 +0100

    no overwriting

--- platoon-dropdown.js
+++ platoon-dropdown.js
@@ -9,14 +9,14 @@
 */
 
 BBLog.handle("add.plugin", {
-    id : "platoon-dropdown",
-    name : "Platoon Dropdown",
+id : "platoon-dropdown",
+name : "Platoon Dropdown",
     
-    configFlags : [
+configFlags : [
             ["plugin.description", 1, function(instance){instance.PlatoonDropdownMenu(instance);}],
     ],
 
-    translations : {
+translations : {
         "en" : {
             "plugin.description" : "Manage displayed platoons",
             "text.description" : "Here you can choose which platoons will be displayed. You can get the ID of a platoon from the URL of its page, for example: in http://battlelog.battlefield.com/bf3/en/platoon/2832655241424190855/, the ID is <strong>2832655241424190855</strong>.",
@@ -36,12 +36,12 @@ BBLog.handle("add.plugin", {
     
 LoadPlatoonList : function(instance) {
     var platoondropdownmenucode = '<div class="section-title customfont">' + instance.t("plugin.description") + '</div><div class="section-description"><p>' + instance.t("text.description") + '</p></div>';
-    var platoondropdownstoredplatoons = instance.storage("platoondropdownstoredplatoons");
-    if(platoondropdownstoredplatoons == null || platoondropdownstoredplatoons=="")
+    var platoondropdownstoredplatoonsmenu = instance.storage("platoondropdownstoredplatoons");
+    if(platoondropdownstoredplatoonsmenu == null || platoondropdownstoredplatoonsmenu=="")
     {}
     else
     {
-      $.each(platoondropdownstoredplatoons, function(index,value) {
+      $.each(platoondropdownstoredplatoonsmenu, function(index,value) {
         var item = value.split("||||");
         platoondropdownmenucode += '<div class="radar" style="color: #fff !important; background-color: transparent !important; border-bottom: 0"><div class="source-url">' + item[0] + ' - ' + item[1] +'</div><span class="bblog-button tiny delete pd-delete" data-pdindex="'+index+'">' + BBLog.t("delete") + '</span></div>';        
       });
@@ -55,7 +55,7 @@ PlatoonDropdownMenu : function(instance) {
     $(".bblog-options > .advanced").fadeIn('slow');
 },
   
- init : function(instance){
+init : function(instance){
         if(BBLog.cache("mode") == "bf3"){}
 	      if(BBLog.cache("mode") == "bf4"){
           if($(".base-section-menu > li[data-page='platoons']").length < 1) {
@@ -65,7 +65,7 @@ PlatoonDropdownMenu : function(instance) {
         instance.AddDropdown(instance);
         },   
 
- domchange : function(instance){
+domchange : function(instance){
         if(BBLog.cache("mode") == "bf3"){}
 	      if(BBLog.cache("mode") == "bf4"){
           if(!$(".base-section-menu > li[data-page='platoons']").length) {
@@ -76,8 +76,8 @@ PlatoonDropdownMenu : function(instance) {
         },     
     
 AddDropdown : function(instance){
-    var platoondropdownstoredplatoons = instance.storage("platoondropdownstoredplatoons");
-    if(platoondropdownstoredplatoons == null || platoondropdownstoredplatoons=="")
+    var platoondropdownstoredplatoonsdropdown = instance.storage("platoondropdownstoredplatoons");
+    if(platoondropdownstoredplatoonsdropdown == null || platoondropdownstoredplatoonsdropdown=="")
     {}
     else
     {
@@ -87,7 +87,7 @@ AddDropdown : function(instance){
             $(".dropdown-bar").append('<div class="dropdown-content" data-for="platoons"></div>');
             $('.dropdown-content[data-for="platoons"]').append('<div class="row"></div>');
             $('.dropdown-content[data-for="platoons"] > .row').append('<nav class="span4 dropdown-menu"></nav>');
-            $.each(platoondropdownstoredplatoons, function(index, value) {
+            $.each(platoondropdownstoredplatoonsdropdown, function(index, value) {
                   var item = value.split("||||");
   				        $('.dropdown-content[data-for="platoons"] > .row > nav').append('<a href="http://battlelog.battlefield.com/bf3/'+BBLog.cache("battlelog.language")+'platoon/'+item[1]+'/" data-pdindex="'+index+'"><i class="icon-white icon-friends2"></i><span>'+item[0]+'</span></a>');           
             });
@@ -96,34 +96,34 @@ AddDropdown : function(instance){
     }
 		
     $(".pd-delete").click(function() {
-			   var index = $(this).attr("data-pdindex");
-         var platoondropdownstoredplatoons = instance.storage("platoondropdownstoredplatoons");
-			   platoondropdownstoredplatoons.splice(index, 1);
-         console.log(platoondropdownstoredplatoons);
-		     instance.storage("platoondropdownstoredplatoons", platoondropdownstoredplatoons);
+	 var index = $(this).attr("data-pdindex");
+         var platoondropdownstoredplatoonsdelete = instance.storage("platoondropdownstoredplatoons");
+	 platoondropdownstoredplatoonsdelete.splice(index, 1);
+         console.log(platoondropdownstoredplatoonsdelete);
+	 instance.storage("platoondropdownstoredplatoons", platoondropdownstoredplatoonsdelete);
          $('.dropdown-content[data-for="platoons"] > .row > nav > a[data-pdindex="' + index + '"]').css("display", "none");
          instance.LoadPlatoonList(instance);
-         if(platoondropdownstoredplatoons == null || platoondropdownstoredplatoons=="") 
+         if(platoondropdownstoredplatoonsdelete == null || platoondropdownstoredplatoonsdelete=="") 
          {
           $(".base-section-menu li[data-page='platoons']").removeClass("has-dropdown");
           $(".base-section-menu li[data-page='platoons']").removeAttr('data-bind-toggle'); 
           $('.dropdown-content[data-for="platoons"]').remove();        
          }
-		});    
+	});    
     $(".pd-add").click(function() {
          var pdname = $(".pd-name").val();
          var pdid = $(".pd-id").val();
          var newitem = pdname + '||||' + pdid;
-         var platoondropdownstoredplatoons = instance.storage("platoondropdownstoredplatoons");
-         if(platoondropdownstoredplatoons == null || platoondropdownstoredplatoons=="") 
+         var platoondropdownstoredplatoonsadd = instance.storage("platoondropdownstoredplatoons");
+         if(platoondropdownstoredplatoonsadd == null || platoondropdownstoredplatoonsadd=="") 
          {
-          platoondropdownstoredplatoons = [newitem];
+          platoondropdownstoredplatoonsadd = [newitem];
          }
          else
          {
-         platoondropdownstoredplatoons.push(newitem);
+         platoondropdownstoredplatoonsadd.push(newitem);
          }
-         instance.storage("platoondropdownstoredplatoons", platoondropdownstoredplatoons);
+         instance.storage("platoondropdownstoredplatoons", platoondropdownstoredplatoonsadd);
          instance.LoadPlatoonList(instance);
          
 		});     
